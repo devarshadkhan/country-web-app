32ee42ac7312e364ca4058f38e7e203271e396ff
codewitharshad <developer.codewitharsh@gmail.com>
2023-09-28 05:27:30 +0530

    check it

--- src/components/SearchContry.jsx
+++ src/components/SearchContry.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { AiOutlineSearch } from 'react-icons/ai'; // Notice the correct import statement
+// import { AiOutlineSearch } from 'react-icons/ai'; // Notice the correct import statement
+import AiOutlineSearch  from "@react-icons/all-files/Ai/AiOutlineSearch";
 
 const SearchCountry = () => {
   return (
