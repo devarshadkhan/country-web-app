8278efd7ab7cdc461121167acfb0ec316029507c
codewitharshad <developer.codewitharsh@gmail.com>
2023-09-28 05:02:26 +0530

    config file changes

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,24 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
+import { defineConfig } from 'vite';
+ import react from '@vitejs/plugin-react'; 
+ // https://vitejs.dev/config/
+ export default defineConfig({   
+    plugins: [react()],   
+    build: {     
+rollupOptions: {   
+    external: [  
+       'react-icons/Bs',     
+    'react-icons/cg',     
+    'react-icons/Fa',     
+    'react-icons/si',     
+  ], // Add this line    
+ }, 
+  }, 
+}); 
