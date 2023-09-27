1aa81ad131e61664c2f85203fbe1dece1b3965c2
codewitharshad <developer.codewitharsh@gmail.com>
2023-09-28 04:54:09 +0530

    config file rechange

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,7 @@
-import vueJsx from "@vitejs/plugin-vue-jsx";
-import { resolve } from "path";
-import { defineConfig } from "vite";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  alias: {
-    "@": resolve(__dirname, "src"),
-  },
-  plugins: [vueJsx()],
-  build: {
-    rollupOptions: {
-      external: [
-        "react", // ignore react stuff
-        "react-dom",
-      ],
-    },
-  },
-  optimizeDeps: {
-    include: ["@apollo/client/core", "@apollo/client/link/error"],
-  },
-});
+  plugins: [react()],
+})
