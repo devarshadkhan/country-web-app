1ce71a9c628c87d18a0cb718a142cdbdc14216fc
codewitharshad <developer.codewitharsh@gmail.com>
2023-09-29 00:22:38 +0530

    config chnages

--- vite.config.js
+++ vite.config.js
@@ -6,34 +6,33 @@
 //   plugins: [react()],
 // })
 
-import { defineConfig } from 'vite';
- import react from '@vitejs/plugin-react'; 
- // https://vitejs.dev/config/
- export default defineConfig({   
-    plugins: [react()], 
-    base: "/country-web-app/",  
-    build: {     
-rollupOptions: {   
-    external: [  
-    'react-icons/Bs',     
-    'react-icons/cg',     
-    'react-icons/Fa',     
-    'react-icons/si',     
-    // 'react-icons/Ai',     
-  ], // Add this line    
- }, 
-  }, 
-}); 
+// import { defineConfig } from 'vite';
+//  import react from '@vitejs/plugin-react'; 
+//  // https://vitejs.dev/config/
+//  export default defineConfig({   
+//     plugins: [react()], 
+//     build: {     
+// rollupOptions: {   
+//     external: [  
+//     'react-icons/Bs',     
+//     'react-icons/cg',     
+//     'react-icons/Fa',     
+//     'react-icons/si',     
+//     // 'react-icons/Ai',     
+//   ], // Add this line    
+//  }, 
+//   }, 
+// }); 
 
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// export default defineConfig({
-//   plugins: [react()],
-//   base: "/country-web-app/",
-//   build: {
-//     rollupOptions: {
-//       external: ['react-icons']
-//     }
-//   }
-// })
+export default defineConfig({
+  plugins: [react()],
+  base: "/country-web-app/",
+  build: {
+    rollupOptions: {
+      external: ['react-icons']
+    }
+  }
+})
