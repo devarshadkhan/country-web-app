a25e9b5b32e02c6edfbeecadfbbbb999d49eba26
codewitharshad <developer.codewitharsh@gmail.com>
2023-09-28 23:54:18 +0530

    file chnages config

--- src/components/SearchContry.jsx
+++ src/components/SearchContry.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
-import { AiOutlineSearch } from 'react-icons/Ai';
+// import { AiOutlineSearch } from 'react-icons/Ai';
 
 const SearchContry = () => {
   return (
     <div>
-      <div className='srchContry'><AiOutlineSearch /> <input type='text' placeholder='search contry' /></div>
+      <div className='srchContry'> <input type='text' placeholder='search contry' /></div>
     </div>
   )
 }
--- vite.config.js
+++ vite.config.js
@@ -1,25 +1,25 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+})
 
-import { defineConfig } from 'vite';
- import react from '@vitejs/plugin-react'; 
- // https://vitejs.dev/config/
- export default defineConfig({   
-    plugins: [react()],   
-    build: {     
-rollupOptions: {   
-    external: [  
-    'react-icons/Bs',     
-    'react-icons/cg',     
-    'react-icons/Fa',     
-    'react-icons/si',     
-    'react-icons/Ai',     
-  ], // Add this line    
- }, 
-  }, 
-}); 
+// import { defineConfig } from 'vite';
+//  import react from '@vitejs/plugin-react'; 
+//  // https://vitejs.dev/config/
+//  export default defineConfig({   
+//     plugins: [react()],   
+//     build: {     
+// rollupOptions: {   
+//     external: [  
+//     'react-icons/Bs',     
+//     'react-icons/cg',     
+//     'react-icons/Fa',     
+//     'react-icons/si',     
+//     'react-icons/Ai',     
+//   ], // Add this line    
+//  }, 
+//   }, 
+// }); 
